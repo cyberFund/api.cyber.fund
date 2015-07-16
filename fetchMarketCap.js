ec819188c89a6eb58371784dbb19082589e59c3c
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2015-07-16 13:33:49 +0600

    putting back 5 minutes interval

--- fetchMarketCap.js
+++ fetchMarketCap.js
@@ -2,90 +2,141 @@ var _ = require('underscore');
 var rp = require('request-promise');
 var esLib = require('elasticsearch');
 var moment = require('moment');
-var config = require("./.config.json");
+var CG = require("./lib/chaingear-fetcher");
+var utils = require("./lib/utils");
+var logger = require("log4js").getLogger("marketCap fetcher");
 
 var es = new esLib.Client({
-
   host: 'http://' + process.env.ES_USERNAME + ':' + process.env.ES_PASSWORD + '@es.index.cyber.fund',
-
-
   //log: 'trace'
 });
 
-
-var sourceUrl = "http://coinmarketcap.northpole.ro/api/v5/all.json";
-var fetchInterval = ("RARE_FETCH" in process.env ? 50 : 5) * 60 * 1000;
-
+var sourceUrlMC = "http://coinmarketcap.northpole.ro/api/v5/all.json";
+var fetchIntervalMC = 5 * 60 * 1000;
 
 var index_old = "market-cap-data";
-var index_v = "marktcap-v2";
+var index_v = "marktcap-v3";
 var alias_read = "marketcap-read";
 var alias_write = "marketcap-write";
 
-
-function fetch() {
+function fetchMC() {
   var options = {
     method: 'GET',
-    uri: sourceUrl,
-    transform: parseResponse
+    uri: sourceUrlMC,
+    transform: utils.parseResponse
   };
-
-  rp(options).then(handleResponse, handleError);
-}
-
-function _flo(val) {
-  try {
-    return parseFloat(val);
-  } catch (e) {
-    return val;
-  }
-}
-
-function parseResponse(data) {
-  var parsedData = {};
-  try {
-    parsedData = JSON.parse(data);
-  } catch (e) {
-
-  }
-  return parsedData;
+  rp(options).then(handleMCResponse, handleError);
 }
 
 function handleError(error) {
   console.log(error);
 }
 
-function convertMarketCapSource(market) {
+function transformMarketCapData(market, cg_item) {
   function pickCurrencies(item) {
     return _.pick(item, ['usd', 'btc']);
   }
 
+  if (!cg_item.token || !cg_item.token.token_symbol) {
+    logger.warn("no proper symbol for " + cg_item.system);
+    logger.warn("CG.symbol is " + cg_item.symbol);
+    return null;
+  }
+
+  var symbol = cg_item.token.token_symbol;
+  var rating_cyber = cg_item.ratings ? (cg_item.ratings.rating || 0) : 0;
+
   var markt = {
-    cap_usd: _flo(market.marketCap.usd),
-    cap_btc: _flo(market.marketCap.btc),
-    price_usd: _flo(market.price.usd),
-    price_btc: _flo(market.price.btc),
-    volume24_btc: _flo(market.volume24.btc),
-    volume24_usd: _flo(market.volume24.usd),
-    system: market.name,
-    symbol: market.symbol,
-    sym_sys: market.symbol+"|"+market.name,
+    cap_usd: utils.tryParseFloat(market.marketCap.usd),
+    cap_btc: utils.tryParseFloat(market.marketCap.btc),
+    price_usd: utils.tryParseFloat(market.price.usd),
+    price_btc: utils.tryParseFloat(market.price.btc),
+    volume24_btc: utils.tryParseFloat(market.volume24.btc),
+    volume24_usd: utils.tryParseFloat(market.volume24.usd),
     tags: market.category,
     ranking_coinmarketcap: market.position,
-    supply_current: _flo(market.availableSupplyNumber),
-    source: "CoinMarketCap"
+
+    supply_current: utils.tryParseFloat(market.availableSupplyNumber),
+    source: "CoinMarketCap",
+    system: cg_item.system,
+    symbol: symbol,
+    sym_sys: symbol + "|" + cg_item.system,
+    genesis_id: cg_item.genesis_id,
+    dependencies: cg_item.dependencies,
+    rating_cyber: rating_cyber
   };
-  return markt
+  if (cg_item.descriptions) {
+    _.extend(markt, {
+      descriptions: {
+        system_type: cg_item.descriptions.system_type,
+        state: cg_item.descriptions.state,
+        tags: cg_item.descriptions.tags
+      }
+    })
+  }
+
+  if (cg_item.consensus) {
+    _.extend(markt, {
+      consensus: {
+        consensus_type: cg_item.consensus.consensus_type,
+        consensus_name: cg_item.consensus.consensus_name,
+        hashing: cg_item.consensus.hashing
+      }
+    })
+  }
+
+  if (cg_item.specs) {
+    if (cg_item.specs.block_time) {
+      markt.specs = markt.specs || {};
+      markt.specs.block_time = utils.tryParseInt(cg_item.specs.block_time)
+    }
+    if (cg_item.specs.txs_confirm) {
+      markt.specs = markt.specs || {};
+      markt.specs.txs_confirm = utils.tryParseInt(cg_item.specs.txs_confirm)
+    }
+  }
+
+  if (cg_item.events) {
+    markt.events = cg_item.events;
+  }
+
+  return markt;
 }
 
-function handleResponse(response) {
+function handleMCResponse(response) {
+  if (!CG.chaingear) return;
+  /**
+   *
+   * @param item = marketcap item
+   *  function is interested in fields 'system', 'symbol'
+   * returns matching chaingear item or false
+   */
+  function matchItemToCG(item) {
+
+    var ret = _.find(CG.chaingear, function (cg_item) {
+      if (!cg_item.aliases) return false;
+      if (!cg_item.token) return false;
+      // match marketcap name with CG marketcap alias
+      return (cg_item.aliases.coinmarketcap == item.name)
+          // match marketcap symbol with:
+          //               # commented out 1. CG marketcap symbol alias;
+          // 2. CG symbol
+        && //((item.symbol == cg_item.aliases.coinmarketcap.symbol) ||
+        (item.symbol == cg_item.token.token_symbol)// )
+    });
+    return ret;
+  }
+
+  if (!response['timestamp']) {
+    logger.warn("no response.timestamp");
+  }
   var timestamp = response['timestamp'];
   timestamp = moment.utc(moment.unix(timestamp)).format("YYYY-MM-DD[T]HH:mm:ss");
-  console.log(timestamp);
+  logger.info("new data fetched from coinmarketcap; timestamped  " + timestamp);
   var markets = response['markets'];
   var exchangeRates = response['currencyExchangeRates'];
   _.each(exchangeRates, function (v, k) {
-    exchangeRates[k] = _flo(v);
+    exchangeRates[k] = utils.tryParseFloat(v);
   });
 
   var bulk = [];
@@ -100,18 +151,24 @@ function handleResponse(response) {
   bulk.push(exchangeRates);
 
   _.each(markets, function (market) {
-    bulk.push({
-      index: {
-        _index: alias_write,
-        _type: 'market'
-      }
-    });
+    var cg_item = matchItemToCG(market);
 
-    var markt = convertMarketCapSource(market);
-    markt.timestamp = timestamp;
-    bulk.push(markt);
+    if (cg_item) {
+      bulk.push({
+        index: {
+          _index: alias_write,
+          _type: 'market'
+        }
+      });
+
+      var markt = transformMarketCapData(market, cg_item);
+      markt.timestamp = timestamp;
+      bulk.push(markt);
+    }
   });
-  console.log("pushing " + bulk.length / 2 + " records to elasticsearch");
+  /*logger.info("pushing " + bulk.length / 2 + " records to elasticsearch");
+  logger.debug("last item: ");
+  logger.info(bulk.pop());*/
   es.bulk({body: bulk});
 }
 
@@ -120,29 +177,54 @@ function recreate() {
   es.indices.create({index: index_v});
 }
 function putmap() {
-  return es.indices.putMapping({
+  var mapping = {
     index: index_v,
     type: "market",
     ignoreConflicts: true,
     body: {
       "market": {
         properties: {
-          "system": {
-            "type": "string",
-            "index": "not_analyzed"
+          "system": utils.esMappingObjects.notAnalyzedString,
+          "symbol": utils.esMappingObjects.notAnalyzedString,
+          "sym_sys": utils.esMappingObjects.notAnalyzedString,
+          "genesis_id": utils.esMappingObjects.notAnalyzedString,
+          "dependencies": utils.esMappingObjects.notAnalyzedString,
+          "descriptions": {
+            "type": "object",
+            "properties": {
+              "system_type": utils.esMappingObjects.notAnalyzedString,
+              "state": utils.esMappingObjects.notAnalyzedString,
+              "tags": utils.esMappingObjects.notAnalyzedString
+            }
           },
-          "symbol": {
-            "type": "string",
-            "index": "not_analyzed"
+          "consensus": {
+            "type": "object",
+            "properties": {
+              "consensus_type": utils.esMappingObjects.notAnalyzedString,
+              "consensus_name": utils.esMappingObjects.notAnalyzedString,
+              "hashing": utils.esMappingObjects.notAnalyzedString
+            }
           },
-          "sym_sys": {
-            "type": "string",
-            "index": "not_analyzed"
+          "specs": {
+            "type": "object",
+            "properties": {
+              "block_time": {"type": "integer"},
+              "txs_confirm": {"type": "integer"}
+            }
+          },
+          "events": {
+            "type": "object",
+            "properties": {
+              "announcement": utils.esMappingObjects.chainGearDate,
+              "genesis": utils.esMappingObjects.chainGearDate
+            }
           }
         }
       }
     }
-  });
+  };
+
+  return es.indices.putMapping(mapping);
 }
 
 var param = process.argv[2];
@@ -156,8 +238,23 @@ if (param == 'map') {
 }
 
 if (!param) {
-  fetch();
-  setInterval(fetch, fetchInterval);
+  CG.start();
+
+  var moo = setInterval(function () {
+    if (CG.chaingear) {
+      clearInterval(moo);
+      fetchMC();
+      setInterval(fetchMC, fetchIntervalMC);
+    }
+    console.log("waiting for chaingear");
+  }, 1000);
 }
-
-
+/*
+curl -XPOST 'http://esserver:9200/_aliases' -d '
+{
+  "actions" : [
+    { "remove" : { "index" : "marktcap-v2", "alias" : "marketcap-write" } },
+    { "add" : { "index" : "marktcap-v3", "alias" : "marketcap-write" } }
+  ]
+}'
+*/
